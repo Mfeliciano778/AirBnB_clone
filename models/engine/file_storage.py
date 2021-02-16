6981983a5bdbfc1d05f10dd1e3cbebc14f9f6b60
AmilcarArmmand <armmand@gmail.com>
2021-02-16 20:31:03 +0000

    updated file_storage.py with All and New methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,15 @@ from models.place import Place
 from models.review import Review
 
 # list or dictionary for case validation (need to add pattern of dict)
-a_dict = {}
+a_dict = {
+    "BaseModel": base_model.BaseModel,
+    "User": user.User,
+    "City": city.City,
+    "State": state.State,
+    "Amenity": amenity.Amenity,
+    "Place": place.Place,
+    "Review": review.Review
+}
 
 class FileStorage:
     """ Serializes and deserializes object instances to JSON files """
@@ -21,13 +29,22 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    
-
-    def all(self):
+    def all(self, cls=None):
         """ Returns the dictionary __objects """
+        if cls is not None:
+            new_obj = {}
+            for key, value in FileStorage.__objects.items():
+                if type(value).__name__ == cls:
+                    new_obj[key] = obj
+                return new_obj
+        else:
+            return self.__objects
 
     def new(self, obj):
         """  sets in __objects the obj with key <obj class name>.id """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
     """ Serializes __objects to the JSON file (path: __file_path) """
@@ -39,3 +56,6 @@ class FileStorage:
         # (only if the JSON file (__file_path) exists
         # otherwise, do nothing. 
         # If the file doesn’t exist, no exception should be raised)
+
+    def delete(self, obj=None):
+        """ Deletes obj from __objects if it exists """
