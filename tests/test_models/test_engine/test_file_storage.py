466ad8cfc380f94d75c449d4444149a5c8eb342f
AmilcarArmmand <armmand@gmail.com>
2021-02-18 01:36:38 +0000

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,7 @@ Unit tests for file_storage.py
 import unittest
 import uuid
 import json
-import pep8
+import pycodestyle
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -21,7 +21,7 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_conformance(self):
         """Test that models/engine/file_storage.py conforms to PEP-8."""
-        style = pep8.StyleGuide(quiet=True)
+        style = pycodestyle.StyleGuide(quiet=True)
         result = style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
@@ -29,33 +29,8 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_conformance(self):
         """Test that tests/test_models/test_engine/test_file_storage.py conforms to PEP-8."""
-        style = pep8.StyleGuide(quiet=True)
+        style = pycodestyle.StyleGuide(quiet=True)
         result = style.check_files(['tests/test_models/test_engine/test_file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-class TestFileStorage(unittest.TestCase):
-    """Tests to check file_storage"""
-
-    def test_init1(self):
-        '''test_init1 - testing basemodel'''
-        base = BaseModel()
-        self.assertIsInstance(base, BaseModel)
-        self.assertIsInstance(base.created_at, time)
-        self.assertIsInstance(base.updated_at, time)
-        self.assertNotIsInstance(base.id, uuid.UUID)
-        self.assertIsInstance(base.id, str)
-
-    def test_str(self):
-        '''test_str - testing string rep'''
-        base2 = BaseModel()
-        string = "[{}] ({}) {}".format(type(base2).__name__,
-                                       base2.id, base2.__dict__)
-        self.assertEqual(str(base2), string)
-
-    def test_save(self):
-        '''test_save - testing save in BaseModel'''
-        base3 = BaseModel()
-        old_base = base2.updated_at
-        base2.save()
-        self.assertNotEqual(old_base, base.updated_at)
