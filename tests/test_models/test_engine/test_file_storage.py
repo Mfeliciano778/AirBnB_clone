baf72d576aaec766b5452428b749cb6ba366fa9b
AmilcarArmmand <armmand@gmail.com>
2021-02-18 01:37:40 +0000

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,6 @@ Unit tests for file_storage.py
 import unittest
 import uuid
 import json
-import pycodestyle
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -17,20 +16,4 @@ from models.review import Review
 from models.base_model import BaseModel
 from datetime import datetime as time
 
-class TestCodeFormat(unittest.TestCase):
-
-    def test_conformance(self):
-        """Test that models/engine/file_storage.py conforms to PEP-8."""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-    def test_conformance(self):
-        """Test that tests/test_models/test_engine/test_file_storage.py conforms to PEP-8."""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['tests/test_models/test_engine/test_file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
 
